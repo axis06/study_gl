0dae6c6ec859d035ea06f9e7ec343bd0ddbb8c69
axis06 <withcomputer@gmail.com>
2018-04-17 15:12:39 +0900

    Initial Commit

---
+++ HelloGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloGL
+//
+//  Created by FurutaKatsumi on 2018/04/17.
+//  Copyright © 2018年 axis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
